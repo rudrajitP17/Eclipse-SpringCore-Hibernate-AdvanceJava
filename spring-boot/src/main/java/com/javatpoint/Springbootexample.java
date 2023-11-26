46246f87c6df2e6b0c6650dc758afcd20bea53b3
Rudrajit Podder <prudrajit@gmail.com>
2023-11-26 20:02:33 +0530

    Seventh

---
+++ spring-boot/src/main/java/com/javatpoint/Springbootexample.java
@@ -0,0 +1,12 @@
+package com.javatpoint;
+
+import org.springframework.boot.SpringApplication;
+
+public class Springbootexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Springbootexample.class, args);
+	}
+
+}
